929137d63e5665b895cee329b46a62e1d427c4c3
Marko Mikulic <mikulic.marko996@gmail.com>
2021-09-07 13:22:57 +0200

    Added user login  helper structs and methods

--- errors.go
+++ errors.go
@@ -3,12 +3,13 @@ package mop_user
 import "errors"
 
 var (
-	ErrFirstNameBlank   = errors.New("first_name_cannot_be_blank")
-	ErrLastNameBlank    = errors.New("last_name_cannot_be_blank")
-	ErrEmailInvalid     = errors.New("email_is_not_valid")
-	ErrEmailTaken       = errors.New("email_already_taken")
-	ErrPasswordTooShort = errors.New("password_too_short")
-	ErrPasswordMismatch = errors.New("password_and_password_repeat_mismatch")
-	ErrInternal         = errors.New("internal_error")
-	ErrHashingPassword  = errors.New("could_not_hash_the_password")
+	ErrFirstNameBlank        = errors.New("first_name_cannot_be_blank")
+	ErrLastNameBlank         = errors.New("last_name_cannot_be_blank")
+	ErrEmailInvalid          = errors.New("email_is_not_valid")
+	ErrEmailTaken            = errors.New("email_already_taken")
+	ErrPasswordTooShort      = errors.New("password_too_short")
+	ErrPasswordMismatch      = errors.New("password_and_password_repeat_mismatch")
+	ErrInternal              = errors.New("internal_error")
+	ErrHashingPassword       = errors.New("could_not_hash_the_password")
+	ErrInvalidMailOrPassword = errors.New("invalid_mail_or_password")
 )
